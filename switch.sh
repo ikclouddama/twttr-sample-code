da92941d4961aab01890a7ca66d4c5c926024f00
root <root@ip-10-0-1-90.ec2.internal>
2022-11-23 01:13:04 +0000

    first commit message

---
+++ switch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+:'
+basic switch with bash '
+echo " Please Enter your operator system"
+read os
+case $os in
+	linux)
+		sudo yum update -y
+		sudo yum install git -y
+		sudo yum install tree -y;;
+	ubuntu)
+		sudo apt-get update;;
+	*)
+		echo "Please enter right os";;
+	esac
+
+
+
